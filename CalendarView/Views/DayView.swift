f6c2f2b541ae3f12cd85652e270dbc2fcf01a4fc
Kevin Li <kevinli29033@gmail.com>
2020-06-12 20:15:16 -0700

    Added date selection delegate method and animation

--- CalendarView/Views/DayView.swift
+++ CalendarView/Views/DayView.swift
@@ -23,6 +23,10 @@ struct DayView: View, CalendarManagerDirectAccess {
         calendar.isDateInToday(day)
     }
 
+    private var isSelected: Bool {
+        selectedDate == day
+    }
+
     var body: some View {
         Text(numericDay)
             .font(.subheadline)
@@ -31,6 +35,8 @@ struct DayView: View, CalendarManagerDirectAccess {
             .background(backgroundColor)
             .clipShape(Circle())
             .opacity(opacity)
+            .overlay(isSelected && isDayWithinWeekMonthAndYear ? CircularSelectionView() : nil)
+            .onTapGesture(perform: notifyManager)
     }
 
     private var numericDay: String {
@@ -45,6 +51,7 @@ struct DayView: View, CalendarManagerDirectAccess {
         }
     }
 
+    // TOOD: Add opacity configuration later on and base the background color opacity off that
     private var backgroundColor: Color {
         if isDayToday {
             return .white
@@ -55,9 +62,37 @@ struct DayView: View, CalendarManagerDirectAccess {
         }
     }
 
-    // TOOD: Add opacity configuration later on and base the background color opacity off that
     private var opacity: Double {
         isDayWithinDateRange && isDayWithinWeekMonthAndYear ? 1 : 0.15
+    }
+
+    private func notifyManager() {
+        if isDayWithinDateRange && isDayWithinWeekMonthAndYear {
+            calendarManager.dayTapped(day: day)
+        }
+    }
+
+}
+
+private struct CircularSelectionView: View {
+
+    @State private var startBounce = false
+
+    var body: some View {
+        Circle()
+            .stroke(Color.white, lineWidth: 2)
+            .frame(width: radius, height: radius)
+            .opacity(startBounce ? 1 : 0)
+            .animation(.interpolatingSpring(stiffness: 150, damping: 10))
+            .onAppear(perform: startBounceAnimation)
+    }
+
+    private var radius: CGFloat {
+        startBounce ? CalendarConstants.dayWidth + 5 : CalendarConstants.dayWidth + 25
+    }
+
+    private func startBounceAnimation() {
+        startBounce = true
     }
 
 }
