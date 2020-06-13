b4f52a61f52d587091059804afd540339fcacc6b
Kevin Li <kevinli29033@gmail.com>
2020-06-12 22:13:28 -0700

    updated calendermanager injection syntax

--- CalendarView/SceneDelegate.swift
+++ CalendarView/SceneDelegate.swift
@@ -14,8 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Create the SwiftUI view that provides the window contents.
-        let calenderView = CalendarView()
-            .environmentObject(CalendarManager(configuration: .mock))
+        let calenderView = CalendarView(calendarManager: CalendarManager(configuration: .mock))
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
