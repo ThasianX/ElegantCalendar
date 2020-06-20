95f5093c8b51ff33990d5f169bea94fbcfcc93f9
Kevin Li <kevinli29033@gmail.com>
2020-06-19 20:49:35 -0700

    Got an efficient calendar paging implementation working. need to fix the contentoffset animation

--- CalendarView/Helpers/Extensions/UIScrollView+Pagination.swift
+++ CalendarView/Helpers/Extensions/UIScrollView+Pagination.swift
@@ -9,10 +9,10 @@ extension UIScrollView {
         backgroundColor = .none
         scrollsToTop = false
 
-        // gets rid of scroll insets
-        contentInset = UIEdgeInsets(top: -CalendarConstants.scrollInsets,
+        // gets rid of scroll insets. iphone 11 only. TODO: Need to test other phones. I should actually grab the scroll insets before I start attaching the scrollview
+        contentInset = UIEdgeInsets(top: -44,
                                     left: 0,
-                                    bottom: -CalendarConstants.scrollInsets,
+                                    bottom: -34,
                                     right: 0)
 
         isPagingEnabled = true
