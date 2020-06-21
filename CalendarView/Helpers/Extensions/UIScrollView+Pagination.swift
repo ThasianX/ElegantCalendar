c5ed946690dc0813ad0ac860c99a5f362df3d976
Kevin Li <kevinli29033@gmail.com>
2020-06-21 15:35:34 -0700

    Fixed yearly scrollview button offset and scrollview insets.somewhat

--- CalendarView/Helpers/Extensions/UIScrollView+Pagination.swift
+++ CalendarView/Helpers/Extensions/UIScrollView+Pagination.swift
@@ -8,6 +8,9 @@ extension UIScrollView {
         backgroundColor = .none
         scrollsToTop = false
 
+        frame = CGRect(x: 0, y: 0,
+                       width: CalendarConstants.cellWidth,
+                       height: CalendarConstants.cellHeight)
         contentInset = UIEdgeInsets(top: -adjustedContentInset.top,
                                     left: 0,
                                     bottom: -adjustedContentInset.bottom,
