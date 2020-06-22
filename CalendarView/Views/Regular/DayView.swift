1e3dadccc3b36b1ddf148f8827ffacc684cd0790
Kevin Li <kevinli29033@gmail.com>
2020-06-22 16:21:53 -0700

    moved color to extension

--- CalendarView/Views/Regular/DayView.swift
+++ CalendarView/Views/Regular/DayView.swift
@@ -2,22 +2,6 @@
 
 import SwiftUI
 
-public extension Color {
-    static let lightText = Color(UIColor.lightText)
-    static let darkText = Color(UIColor.darkText)
-
-    static let label = Color(UIColor.label)
-    static let secondaryLabel = Color(UIColor.secondaryLabel)
-    static let tertiaryLabel = Color(UIColor.tertiaryLabel)
-    static let quaternaryLabel = Color(UIColor.quaternaryLabel)
-
-    static let systemBackground = Color(UIColor.systemBackground)
-    static let secondarySystemBackground = Color(UIColor.secondarySystemBackground)
-    static let tertiarySystemBackground = Color(UIColor.tertiarySystemBackground)
-
-    // There are more..
-}
-
 struct DayView: View, MonthlyCalendarManagerDirectAccess {
 
     @EnvironmentObject var calendarManager: MonthlyCalendarManager
