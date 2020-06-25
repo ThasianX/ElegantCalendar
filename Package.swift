a7dc3f43fa6446450652f4ecf934fdf6d07660a7
Kevin Li <kevinli29033@gmail.com>
2020-06-24 19:30:36 -0700

    Converted project to swift package

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ElegantCalendar",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "ElegantCalendar",
+            targets: ["ElegantCalendar"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ThasianX/ElegantPages", .upToNextMajor(from: "1.0.0"))
+    ],
+    targets: [
+        .target(
+            name: "ElegantCalendar",
+            dependencies: ["ElegantPages"])
+    ]
+)
