3a7e680102a280d6753e9d0e2b7b0f4395fdcce0
Kevin Li <kevinli29033@gmail.com>
2020-07-13 21:02:23 -0700

    Updated the design of the calendar.
    commit d1a5461cfec1d3fa1a96a424b52270ad53a35590
    Merge: 848285d d661e79
    Author: Kevin Li <kevinli29033@gmail.com>
    Date:   Mon Jul 13 21:01:08 2020 -0700
    
        Merge branch 'master' into feature/appearance-refinement
    
    commit 848285db8df4e17783002838f5a3ba6fe8afd76a
    Author: Kevin Li <kevinli29033@gmail.com>
    Date:   Mon Jul 13 20:59:35 2020 -0700
    
        Update README.md
    
    commit 0df0a2f1cd285054d8ef92bfb1526d5698d399d6
    Author: Kevin Li <kevinli29033@gmail.com>
    Date:   Mon Jul 13 20:53:18 2020 -0700
    
        Update README.md
    
    commit ddbaeb7d767d4063a50377eee64d0f41563812c3
    Author: Kevin Li <kevinli29033@gmail.com>
    Date:   Mon Jul 13 20:46:26 2020 -0700
    
        added fatalerror warning for custom image
    
    commit 8a40df1b35d23622fb5fe9c504b27fb4299f87eb
    Author: Kevin Li <kevinli29033@gmail.com>
    Date:   Mon Jul 13 20:44:57 2020 -0700
    
        hotfix for custom icon
    
    commit 85233797fdad39e37eff3c3c68d986ef7d2b9ff1
    Author: Kevin Li <kevinli29033@gmail.com>
    Date:   Mon Jul 13 17:08:07 2020 -0700
    
        updated readme
    
    commit fd9c6a5e26ccb2353a36d1a211c2c64dc29937cb
    Author: Kevin Li <kevinli29033@gmail.com>
    Date:   Mon Jul 13 17:01:07 2020 -0700
    
        new gifs
    
    commit 65e8f2ffdb753d4ac77af2623e32b6b974fd42cb
    Author: Kevin Li <kevinli29033@gmail.com>
    Date:   Mon Jul 13 16:27:01 2020 -0700
    
        better monthly paging animation
    
    commit 23359432cffbfc916d5c4393eed844bee7a4f14b
    Author: Kevin Li <kevinli29033@gmail.com>
    Date:   Mon Jul 13 16:22:16 2020 -0700
    
        fixed the yearly calendar scroll button alignment
    
    commit 54ef970c717b5499f97fe092fc4caf34b20b9f22
    Author: Kevin Li <kevinli29033@gmail.com>
    Date:   Mon Jul 13 16:15:39 2020 -0700
    
        scroll to today button is now aligned for monthly view
    
    commit d3c9ad0f5386f7fdd8499990dc68d7400bd0b9e5
    Author: Kevin Li <kevinli29033@gmail.com>
    Date:   Mon Jul 13 16:01:48 2020 -0700
    
        a more inline header and accessory view
    
    commit 3f2371a4cca9019a917332237c004011eb27c6d1
    Author: Kevin Li <kevinli29033@gmail.com>
    Date:   Mon Jul 13 15:57:40 2020 -0700
    
        better looking month header
    
    commit 424bcff29a8a59e02269ed2a080c652200fb1a65
    Author: Kevin Li <kevinli29033@gmail.com>
    Date:   Mon Jul 13 15:48:45 2020 -0700
    
        made the day view font smaller
    
    commit 4f1f0f45d545a21f4de189286514a3a78b58aab1
    Author: Kevin Li <kevinli29033@gmail.com>
    Date:   Mon Jul 13 15:45:54 2020 -0700
    
        added custom scroll image ripped straight from timepage lmao

---
+++ Sources/ElegantCalendar/Helpers/Extensions/Image+Custom.swift
@@ -0,0 +1,14 @@
+// Kevin Li - 3:24 PM - 7/13/20
+
+import SwiftUI
+
+extension Image {
+
+    static var uTurnLeft: Image {
+        guard let image = UIImage(named: "uturn.left") else {
+            fatalError("Error: `ElegantCalendar.xcassets` doesn't exist. Refer to the `README.md` installation on how to resolve this.")
+        }
+        return Image(uiImage: image)
+    }
+
+}
