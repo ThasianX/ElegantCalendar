ddbaeb7d767d4063a50377eee64d0f41563812c3
Kevin Li <kevinli29033@gmail.com>
2020-07-13 20:46:26 -0700

    added fatalerror warning for custom image

--- Sources/ElegantCalendar/Helpers/Extensions/Image+Custom.swift
+++ Sources/ElegantCalendar/Helpers/Extensions/Image+Custom.swift
@@ -5,7 +5,9 @@ import SwiftUI
 extension Image {
 
     static var uTurnLeft: Image {
-        let image = UIImage(named: "uturn.left")!
+        guard let image = UIImage(named: "uturn.left") else {
+            fatalError("Error: `ElegantCalendar.xcassets` doesn't exist. Refer to the `README.md` installation on how to resolve this.")
+        }
         return Image(uiImage: image)
     }
 
