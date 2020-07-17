dbd7c4fc330436a13876f2b44c3c4c7c17633cb7
Kevin Li <kevinli29033@gmail.com>
2020-07-17 00:32:59 -0700

    new elegantpages

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["ElegantCalendar"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ThasianX/ElegantPages", .branch("feature/syntax"))
+        .package(url: "https://github.com/ThasianX/ElegantPages", from: "1.4.0")
     ],
     targets: [
         .target(
